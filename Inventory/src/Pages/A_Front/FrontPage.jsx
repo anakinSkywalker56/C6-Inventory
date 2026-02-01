6879fcba30ab019bc43d52fc4adca517d513c587
Anakin <johninjug56@gmail.com>
2026-02-01 12:27:19 +0800

    Initial setup for main page, adding Hello World as placeholder for testing if it works, and it does!

---
+++ Inventory/src/Pages/A_Front/FrontPage.jsx
@@ -0,0 +1,11 @@
+const FrontPage = () => {
+  return (
+    <div className="flex">
+      <div className="flex h-full w-screen justify-center items-center">
+        <div className="text-2xl">Hello World</div>
+      </div>
+    </div>
+  );
+};
+
+export default FrontPage;
