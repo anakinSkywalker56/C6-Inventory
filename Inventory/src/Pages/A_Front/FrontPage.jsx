f17a8ecb8309fe731ee86d874aa67396fa131838
Anakin <johninjug56@gmail.com>
2026-02-26 11:44:14 +0800

    changes were automatic after clearing cache and reinstalling react-router-dom

--- Inventory/src/Pages/A_Front/FrontPage.jsx
+++ Inventory/src/Pages/A_Front/FrontPage.jsx
@@ -1,16 +1,30 @@
 import Header from "../../Components/General/Header";
+import { useNavigate } from "react-router";
 
-const FrontPage = () => {
+const Front = () => {
+  const navigate = useNavigate();
+  const handleCreate = () => {
+    navigate("/create");
+  };
+  const handleOpen = () => {
+    navigate("/");
+  };
   return (
     <div className="flex">
       <div className="flex h-full w-screen justify-center items-center">
         <div className="text-2xl">
           <Header />
-          <div className="flex flex-col gap-5 justify-center items-center w-screen h-screen">
-            <div className="h-40 w-120 bg-blue-500 text-4xl flex justify-center items-center text-white rounded-2xl">
+          <div className="flex flex-col gap-5 justify-center items-center w-full h-screen">
+            <div
+              className="h-40 w-120 bg-blue-500 text-4xl flex justify-center items-center text-white rounded-2xl"
+              onClick={handleCreate}
+            >
               Create New Database
             </div>
-            <div className="h-40 w-120 bg-green-500 text-4xl flex justify-center items-center text-white rounded-2xl">
+            <div
+              className="h-40 w-120 bg-green-500 text-4xl flex justify-center items-center text-white rounded-2xl"
+              onClick={handleOpen}
+            >
               Open Existing Database
             </div>
           </div>
@@ -20,4 +34,4 @@ const FrontPage = () => {
   );
 };
 
-export default FrontPage;
+export default Front;
