62250a06cdfcc287ab5c7fc63f6ed29b39faa050
Anakin <johninjug56@gmail.com>
2026-02-26 11:45:23 +0800

    Created New Page to start Creation function in the system

---
+++ Inventory/src/Pages/B_Create/CreateDB.jsx
@@ -0,0 +1,15 @@
+import Header from "../../Components/General/Header";
+
+const CreateDB = () => {
+  return (
+    <div className="flex">
+      <div className="flex h-full w-screen justify-center items-center">
+        <div className="text-2xl">
+          <Header />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CreateDB;
